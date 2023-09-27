42b7f1db673d166c23127a318f0f414ea3a72c5e
ltamit <amitwalia@lambdatest.com>
2023-09-27 16:01:15 +0530

    Extent Report

---
+++ src/main/java/AndroidApp.java
@@ -0,0 +1,152 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+public class AndroidApp {
+
+    String userName = System.getenv("LT_USERNAME") == null ?
+            "username" : System.getenv("LT_USERNAME"); //Add username here
+    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
+            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+
+    public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
+
+    AppiumDriver driver;
+
+
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @BeforeSuite
+    public void beforeSuite() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/ExtentReports.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        extent.flush();
+    }
+
+
+    @Test
+    @org.testng.annotations.Parameters(value = {"device", "version", "platform"})
+    public void AndroidApp1(String device, String version, String platform) throws Exception {
+        ExtentTest extentTest = extent.createTest(platform + " " + device + " " + version);
+        test.set(extentTest);
+        try {
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("build", "Java TestNG Android");
+            capabilities.setCapability("name", platform + " " + device + " " + version);
+            capabilities.setCapability("deviceName", device);
+            capabilities.setCapability("platformVersion", version);
+            capabilities.setCapability("platformName", platform);
+            capabilities.setCapability("isRealMobile", true);
+            //AppURL (Create from Wikipedia.apk sample in project)
+            capabilities.setCapability("app", "lt://APP101606261692684794672801"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+            //capabilities.setCapability("geoLocation", "HK");
+
+            String hub = "https://" + userName + ":" + accessKey + gridURL;
+            driver = new AppiumDriver(new URL(hub), capabilities);
+
+            MobileElement color = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/color");
+            //Changes color to pink
+            color.click();
+            Thread.sleep(1000);
+            //Back to orginal color
+            color.click();
+
+            MobileElement text = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/Text");
+            //Changes the text to "Proverbial"
+            text.click();
+
+            //toast will be visible
+            MobileElement toast = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/toast");
+            toast.click();
+
+            //notification will be visible
+            MobileElement notification = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/notification");
+            notification.click();
+            Thread.sleep(2000);
+
+            //Opens the geolocation page
+            MobileElement geo = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/geoLocation");
+            geo.click();
+            Thread.sleep(5000);
+
+            //takes back to home page
+            MobileElement home = (MobileElement) driver.findElementByAccessibilityId("Home");
+            home.click();
+
+            //Takes to speed test page
+            MobileElement speedtest = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/speedTest");
+            speedtest.click();
+            Thread.sleep(5000);
+
+            MobileElement Home = (MobileElement) driver.findElementByAccessibilityId("Home");
+            Home.click();
+
+            //Opens the browser
+            MobileElement browser = (MobileElement) driver.findElementByAccessibilityId("Browser");
+            browser.click();
+
+            MobileElement url = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/url");
+            url.sendKeys("https://www.lambdatest.com");
+
+            MobileElement find = (MobileElement) driver.findElementById("com.lambdatest.proverbial:id/find1");
+            find.click();
+
+            driver.quit();
+
+
+            // ... [rest of the test method code] ...
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            test.get().log(Status.FAIL, "Test failed due to: " + e.getMessage(),
+                    MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+            try {
+                driver.quit();
+            } catch (Exception e1) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getScreenshot() throws Exception {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
+        File destination = new File(path);
+        FileUtils.copyFile(srcFile, destination);
+        return path;
+    }
+}
+
